ea38ceb8a06ee2df54532701775a0b9ce0e87c9e
Taras <vbpiar@gmail.com>
2019-12-09 15:15:28 +0200

    Ad migrations for models

---
+++ youtube/database/migrations/2019_12_08_155908_create_channels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('youtube_token');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('last_upload')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channel');
+    }
+}
